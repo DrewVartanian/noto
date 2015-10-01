11cdb29ba33d44a9d8198e7432e23aa13820b837
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-10-01 19:43:18 -0400

    add note added to right click

--- extension/background/background.js
+++ extension/background/background.js
@@ -11,7 +11,20 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   }
 });
 
-// chrome.browserAction.onClicked.addListener(function(activeTab){
-//   var newURL = "popup.html";
-//   chrome.tabs.create({ url: newURL });
-// });
+chrome.runtime.onInstalled.addListener(function() {
+  var context = "selection";
+  var title = "Add New Note";
+  var id = chrome.contextMenus.create({"title": title,"contexts":["all"]});
+  alert('addListener');
+});
+
+// add click event
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+  alert('onClickHandler');
+  var sText = info.selectionText;
+  var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);
+  window.open(url, '_blank');
+};
