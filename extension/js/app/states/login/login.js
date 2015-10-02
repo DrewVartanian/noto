421734407a71ceefcde4ed3eef22b7b4ba7794b6
Mark Pytel <markpytel@gmail.com>
2015-10-01 21:20:00 -0400

    login working inside extension

---
+++ extension/js/app/states/login/login.js
@@ -0,0 +1,29 @@
+app.config(function ($stateProvider) {
+
+    $stateProvider.state('login', {
+        url: '/login',
+        templateUrl: 'js/app/states/login/login.html',
+        controller: 'LoginCtrl'
+    });
+
+});
+
+app.controller('LoginCtrl', function ($scope, AuthService, $state) {
+
+    $scope.login = {};
+    $scope.error = null;
+
+    $scope.sendLogin = function (loginInfo) {
+
+        $scope.error = null;
+
+        AuthService.login(loginInfo).then(function () {
+            console.log("Success");
+            //$state.go('home');
+        }).catch(function () {
+            $scope.error = 'Invalid login credentials.';
+        });
+
+    };
+
+});
