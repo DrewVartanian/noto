1fe0dddf9e1661fd899f2a4fc9c86068a60819fe
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-10-01 20:03:19 -0400

    background run through html to allow for console logs

--- extension/background/background.js
+++ extension/background/background.js
@@ -10,12 +10,11 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {code:"alert()"});
   }
 });
-
+console.log('background js');
 chrome.runtime.onInstalled.addListener(function() {
   var context = "selection";
   var title = "Add New Note";
   var id = chrome.contextMenus.create({"title": title,"contexts":["all"]});
-  alert('addListener');
 });
 
 // add click event
@@ -23,7 +22,7 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // The onClicked callback function.
 function onClickHandler(info, tab) {
-  alert('onClickHandler');
+  console.dir(info);
   var sText = info.selectionText;
   var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);
   window.open(url, '_blank');
