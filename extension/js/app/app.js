861baf70276a68ff4a70e8d10308a026c21b5bb6
= <danrlabrie@gmail.com>
2015-10-02 17:38:31 -0400

    signup posts correctly, no persistence yet

--- extension/js/app/app.js
+++ extension/js/app/app.js
@@ -9,6 +9,10 @@ app.config(function ($urlRouterProvider, $locationProvider) {
     });
     // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
     $urlRouterProvider.otherwise('/');
+    
+    $urlRouterProvider.when('/auth/:provider', function () {
+   window.location.reload();
+});
 });
 
 // This app.run is for controlling access to specific states.
--- extension/js/app/states/login/login.js
+++ extension/js/app/states/login/login.js
@@ -12,7 +12,12 @@ app.controller('LoginCtrl', function ($scope, AuthService, $state) {
 
     $scope.login = {};
     $scope.error = null;
-
+    $scope.google = function () {
+        AuthService.google().then(function () {
+            console.log("success!");
+            window.location='http://127.0.0.1:1337/auth/google';
+        });
+    };
     $scope.sendLogin = function (loginInfo) {
 
         $scope.error = null;
@@ -26,4 +31,17 @@ app.controller('LoginCtrl', function ($scope, AuthService, $state) {
 
     };
 
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
 });
