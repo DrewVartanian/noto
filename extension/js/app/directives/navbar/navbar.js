12197e5b5f8224a9494ff288ebf8e678b1f63b42
Mark Pytel <markpytel@gmail.com>
2015-10-05 15:47:09 -0400

    pageview working with new login process

--- extension/js/app/directives/navbar/navbar.js
+++ extension/js/app/directives/navbar/navbar.js
@@ -1,45 +1,56 @@
-app.directive('navBar', function ($rootScope, $state, BackgroundFactory, $log) {
-    
+app.directive('navBar', function($rootScope, $state, BackgroundFactory, $log, ExtensionFactory) {
+
     return {
         restrict: 'E',
         scope: {},
         templateUrl: '/js/app/directives/navbar/navbar.html',
-        link: function (scope) {
-            
-            scope.items = [
-                { label: 'Home', state: 'webnote' },
-                { label: 'Manage Circles', state: 'circles' },
-                { label: 'Account Management', state: 'account' }
-            ];
+        link: function(scope) {
+
+            scope.items = [{
+                label: 'Home',
+                state: 'webnote'
+            }, {
+                label: 'Manage Circles',
+                state: 'circles'
+            }, {
+                label: 'Account Management',
+                state: 'account'
+            }];
 
             scope.user;
 
-            $rootScope.$on('nicknameChange', function (event, nickname) {
+            $rootScope.$on('nicknameChange', function(event, nickname) {
                 if (scope.user) {
                     scope.user.nickname = nickname;
                 };
             })
-            scope.logout = function () {
+            scope.logout = function() {
                 BackgroundFactory.logOutUser()
-                .then(function (statusCode) {
-                    $state.go('login');                
-                    $rootScope.isLoggedIn = false;
-                    scope.user = null;
-                })
-                .catch(function(err) {
-                    $log.warn(err);
-                })
+                    .then(function(statusCode) {
+                        $state.go('login');
+                        $rootScope.isLoggedIn = false;
+                        scope.user = null;
+                    })
+                    .catch(function(err) {
+                        $log.warn(err);
+                    })
             };
 
-            var showUserOnNavbar = function () {
+            var showUserOnNavbar = function() {
                 BackgroundFactory.checkLoggedIn()
-                .then(function (response) {
-                    var userLoggedIn = response.user;
-                    scope.user = userLoggedIn;
-                })
-                .catch(function(err) {
-                    $log.warn(err);
-                })
+                    .then(function(response) {
+                        var userLoggedIn = response.user;
+                        scope.user = userLoggedIn;
+                        return userLoggedIn;
+                    }).then(function(user) {
+                        ExtensionFactory.getPages(user).then(function(pages) {
+                            //console.log(pages);
+                            scope.pages = pages;
+                        });
+                    })
+                    .catch(function(err) {
+                        $log.warn(err);
+                    })
             };
 
             showUserOnNavbar();
--- server/app/routes/user.router.js
+++ server/app/routes/user.router.js
@@ -12,7 +12,21 @@ var User = mongoose.model('User');
 
 // fsa-pre-built has: /login, /logout
 
+router.get('/:userId', function (req, res){
 
+    var userId = req.params.userId;
+
+    Team.find({users: userId}).exec().then(function(teams){
+        // res.status(200).json(teams);
+        //return teams;
+        Page.find({team: {$in: teams}}).exec().then(function(pages){
+            res.status(200).json(pages);
+        })
+    });
+
+
+
+});
 
 
 
