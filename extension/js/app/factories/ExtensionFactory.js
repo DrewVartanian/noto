5ae39a5f5f3485b5d4637d27404904c8b8c34b20
Mark Pytel <markpytel@gmail.com>
2015-10-07 12:22:15 -0400

    last tweak

--- extension/js/app/factories/ExtensionFactory.js
+++ extension/js/app/factories/ExtensionFactory.js
@@ -2,13 +2,21 @@ app.factory('ExtensionFactory', function ($http){
 	
 	var server = 'http://127.0.0.1:1337';
 
+	var composeRequest = function (method, url, data) {
+        return {
+            method: method,
+            url: server + url,
+            data: data
+        };
+    };
 
 	return {
 		getPages: function() {
-			return $http.get(server+'/api/user/page')
+			return $http(composeRequest('GET', '/api/user/page'))
 			.then(function (res){
 				return res.data;
 			});
 		};
 	}
 });
+
