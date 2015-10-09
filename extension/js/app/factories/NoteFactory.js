c1976cb265144f205c3c502977638f82f8178a13
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-10-08 23:19:03 -0400

    made request factory with url

--- extension/js/app/factories/NoteFactory.js
+++ extension/js/app/factories/NoteFactory.js
@@ -1,50 +1,41 @@
-app.factory('NoteFactory', function ($http,BackgroundFactory) {
+app.factory('NoteFactory', function ($http,BackgroundFactory, RequestFactory) {
 
     var backgroundPage = BackgroundFactory.getBackgroundPage();
     var currentUser = backgroundPage.user;
-    var server = 'http://127.0.0.1:1337';
-
-    var composeRequest = function (method, url, data) {
-        return {
-            method: method,
-            url: server + url,
-            data: data
-        };
-    };
 
     return {
         // note: this route uses req.user. re-wire to currentUser?
         // notes written by current user
         getMyNotes: function() {
-            return $http(composeRequest('GET', '/api/note/user'))
+            return $http(RequestFactory.composeRequest('GET', '/api/note/user'))
             .then(function (response){
                 return response.data;
             });
         },
 
         getNote: function(noteID) {
-            return $http(composeRequest('GET', '/api/note/' + noteID))
+            return $http(RequestFactory.composeRequest('GET', '/api/note/' + noteID))
             .then(function (response){
                 return response.data;
             });
         },
 
         createNote: function(noteInfo) {
-            return $http(composeRequest('POST', '/api/note', noteInfo))
+            return $http(RequestFactory.composeRequest('POST', '/api/note', noteInfo))
             .then(function (response){
                 return response.data;
             });
         },
 
         updateNote: function(noteID, noteInfo) {
-            return $http(composeRequest('PUT', '/api/note/' + noteID, noteInfo))
+            return $http(RequestFactory.composeRequest('PUT', '/api/note/' + noteID, noteInfo))
             .then(function (response){
                 return response.data;
             });
         },
 
         deleteNote: function(noteID) {
-            return $http(composeRequest('DELETE', '/api/note/' + noteID))
+            return $http(RequestFactory.composeRequest('DELETE', '/api/note/' + noteID))
             .then(function (response) {
               return response.data;
             });
--- extension/js/app/factories/PageFactory.js
+++ extension/js/app/factories/PageFactory.js
@@ -1,15 +1,5 @@
 app.factory('PageFactory', function ($http, BackgroundFactory) {
 
-    var server = 'http://127.0.0.1:1337';
-
-    var composeRequest = function (method, url, data) {
-        return {
-            method: method,
-            url: server + url,
-            data: data
-        };
-    };
-
     return {
         getMyPages: function() {
             return BackgroundFactory.getBackgroundPage().pagesProm;
