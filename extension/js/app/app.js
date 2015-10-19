fe530be8a472ddf1945fa03f1ca898c0ad996793
Connie <connie-github@users.noreply.github.com>
2015-10-19 13:43:59 -0400

    add ngMaterial

--- extension/js/app/app.js
+++ extension/js/app/app.js
@@ -1,5 +1,5 @@
 'use strict';
-window.app = angular.module('WebNotes', ['ui.router', 'ui.bootstrap', 'fsaPreBuilt', 'ngAnimate']);
+window.app = angular.module('WebNotes', ['ui.router', 'ui.bootstrap', 'fsaPreBuilt', 'ngAnimate', 'ngMaterial']);
 
 app.config(function($urlRouterProvider, $locationProvider) {
   // This turns off hashbang urls (/#about) and changes it to something normal (/about)
