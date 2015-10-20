5aabf10717e00e0f4dcd9f627486af242c3cc531
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-10-20 17:02:46 -0400

    chrome store url change/gitignore

--- extension/js/app/factories/RequestFactory.js
+++ extension/js/app/factories/RequestFactory.js
@@ -1,6 +1,7 @@
 app.factory('RequestFactory', function() {
 
   var server = 'http://127.0.0.1:1337';
+  // var server = 'https://hidden-everglades-5386.herokuapp.com';
 
   return {
     composeRequest: function(method, url, data) {
